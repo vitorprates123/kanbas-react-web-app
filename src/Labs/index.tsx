293935176b11369e9f077aaafb417e26f37e40f1
Vitor Prates <prates.v@northeastern.edu>
2024-10-22 16:29:04 -0400

    final additions

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -10,7 +10,7 @@ export default function Labs() {
             <h1>Labs-Vitor Prates Section: T/F 3:25 </h1>
             <TOC/>
             <Routes>
-                <Route path="/" element={<Navigate to="Lab2"/>}/>
+                <Route path="/" element={<Navigate to="Lab3"/>}/>
                 <Route path="Lab1" element={<Lab1/>}/>
                 <Route path="Lab2" element={<Lab2/>}/>
                 <Route path="Lab3/*" element={<Lab3 />} />
